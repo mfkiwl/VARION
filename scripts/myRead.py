6bba22a2351d8c8bafac89eaf33738e0d62b23ab
Giorgio Savastano <giorgio.savastano@uniroma1.it>
2016-04-03 14:27:54 -0700

    myRead.py
    my read modify and VARION_next_reading.py

--- scripts/myRead.py
+++ scripts/myRead.py
@@ -1,16 +1,15 @@
 import numpy as np
+import datetime
 import re
 ################################################################################
-def iono(RINEX_file, sat):
+def read_rinex(RINEX):
     """
-    This function read a RINEX file.
     input:
-        - RINEX file
+        - list of observation files
         - name of the sat (e.g. "G18")
-    outputs:
-        - sod array
-        - L1 array
-        - L2 array
+    output a list with:
+        - lista_sat[i][0] the sod of the i file
+        - lista_sat[i][1] the Iono_vad of the i file
     """
 
     L1 = 1.57542e9    # Hz
@@ -20,14 +19,14 @@ def iono(RINEX_file, sat):
     lam1=c/L1         # m
     lam2=c/L2         # m
 
-
-    f   = open(RINEX_file, "r")
+    f   = open(RINEX, "r")
     lns = f.readlines()
     f.close()
     
     sats = []
     ora  = []
     sod  = []
+    c1   = []
     l1   = []
     l2   = []
 
@@ -69,7 +68,7 @@ def iono(RINEX_file, sat):
                         s_sats+=1
                     else:
                         pass
-                                                   
+                                                    
             if (len(ln[-1]) - 1) % 3 == 0:                                 # if the last element (eg 9G27G01G...), it means that n_sats < 10, skip only the first character of the element
                     n_sats = int(ln[-1][0])                                # the first character is the number of the satellites observed in that epoch e.g 9
                     for k in xrange(1,(n_sats-s_sats)*2+1,2):              # e.g. k in [1,3,5,7,9,...,13] I am skipping the last satellites because thew are always
@@ -78,13 +77,21 @@ def iono(RINEX_file, sat):
                     
                         if len(obs) <= 3: continue
                         ##
-                        l1.append(float(obs[l1_index])*lam1)
-                        l2.append(float(obs[l2_index])*lam2)                     # In this RINEX format L1 and L2 are the first two numbers
+                        
+                        c1_before_dec, c1_after_dec = str(obs[c1_index]).split('.')     # split the obs and truncate the number to the 3rd decimal (no rounding)                        
+                        l1_before_dec, l1_after_dec = str(obs[l1_index]).split('.')     # split the obs and truncate the number to the 3rd decimal (no rounding)
+                        l2_before_dec, l2_after_dec = str(obs[l2_index]).split('.')
+                        
+                        c1.append(   float('.'.join(( c1_before_dec, c1_after_dec[0:3])))   )    
+                        l1.append(   float('.'.join(( l1_before_dec, l1_after_dec[0:3])))  *lam1   )
+                        l2.append(   float('.'.join(( l2_before_dec, l2_after_dec[0:3])))  *lam2   )                     # In this RINEX format L1 and L2 are the first two numbers
+
                         
                         sats.append(ln[-1][1:len(ln[-1])][(k)/2*3:(k)/2*3+3])      # skip only the first character (slicing)
-                        ora.append(ln[3] + ":" + ln[4] + ":" + ln[5][0:2])
+                        timing = (  datetime.time( int(ln[3]), int(ln[4]) , int(float(ln[5][0:2])) ) )
+                        ora.append ( timing.isoformat() )
                         sod.append(int(ln[3])*60*60 + int(ln[4])*60 + float(ln[5][0:3]))
-                          
+                            
             elif (len(ln[-1]) - 2) % 3 == 0:                                   # skip the first two characters because the sat number is > 10
                     n_sats = int(ln[-1][0:2])
                     
@@ -95,11 +102,17 @@ def iono(RINEX_file, sat):
                             
                             if len(obs) <= 3: continue
                             ##
-                            l1.append(  float(obs[l1_index])*lam1  )
-                            l2.append(  float(obs[l2_index])*lam2  )                        # In this RINEX format L1 and L2 are the first two numbers
-                            
+                            c1_before_dec, c1_after_dec = str(obs[c1_index]).split('.')     # split the obs and truncate the number to the 3rd decimal (no rounding)                        
+                            l1_before_dec, l1_after_dec = str(obs[l1_index]).split('.')     # split the obs and truncate the number to the 3rd decimal (no rounding)
+                            l2_before_dec, l2_after_dec = str(obs[l2_index]).split('.')
+                        
+                            c1.append(   float('.'.join(( c1_before_dec, c1_after_dec[0:3])))   )    
+                            l1.append(   float('.'.join(( l1_before_dec, l1_after_dec[0:3])))  *lam1   )
+                            l2.append(   float('.'.join(( l2_before_dec, l2_after_dec[0:3])))  *lam2   )                     # In this RINEX format L1 and L2 are the first two numbers
+
                             sats.append(ln[-1][2:len(ln[-1])][(k)/2*3:(k)/2*3+3])     # skip the first two elements
-                            ora.append(ln[3] + ":" + ln[4] + ":" + ln[5][0:2])
+                            timing = (  datetime.time( int(ln[3]), int(ln[4]) , int(float(ln[5][0:2])) ) )
+                            ora.append ( timing.isoformat() )
                             sod.append(int(ln[3])*60*60 + int(ln[4])*60 + float(ln[5][0:3]))
                             
                     else:                                                      # Check the number of sats, if >12 use continuation lines
@@ -109,21 +122,131 @@ def iono(RINEX_file, sat):
                             obs = re.findall('\S+\.\S+',lns[i+1+k])          # use regular expression to select only the float number
                             
                             if len( obs ) <= 3: continue
-                            l1.append(  float(obs[l1_index])*lam1  )
-                            l2.append(  float(obs[l2_index])*lam2  )
+                            c1_before_dec, c1_after_dec = str(obs[c1_index]).split('.')     # split the obs and truncate the number to the 3rd decimal (no rounding)                        
+                            l1_before_dec, l1_after_dec = str(obs[l1_index]).split('.')     # split the obs and truncate the number to the 3rd decimal (no rounding)
+                            l2_before_dec, l2_after_dec = str(obs[l2_index]).split('.')
+                        
+                            c1.append(   float('.'.join(( c1_before_dec, c1_after_dec[0:3])))   )    
+                            l1.append(   float('.'.join(( l1_before_dec, l1_after_dec[0:3])))  *lam1   )
+                            l2.append(   float('.'.join(( l2_before_dec, l2_after_dec[0:3])))  *lam2   )                     # In this RINEX format L1 and L2 are the first two numbers
+
                                     
-                            ora.append(ln[3] + ":" + ln[4] + ":" + ln[5][0:2])
+                            timing = (  datetime.time( int(ln[3]), int(ln[4]) , int(float(ln[5][0:2])) ) )
+                            ora.append ( timing.isoformat() )
                             sod.append(int(ln[3])*60*60 + int(ln[4])*60 + float(ln[5][0:3]))
                                     
                             if k <= 23:
                                         sats.append(ln[-1][2:len(ln[-1])][(k)/2*3:(k)/2*3+3])     # skip the first two elements
                             else:
                                         sats.append(ln_2[0][((k+1)/2-13)*3:((k+1)/2-13)*3+3])
-                                          
-    sats_arr = np.asarray(sats)
+    sats_arr = np.asarray(sats)                                          
     ora_arr  = np.asarray(ora)
     sod_arr  = np.asarray(sod)
+    c1_arr = np.asarray(c1)
     l1_arr = np.asarray(l1)
     l2_arr = np.asarray(l2)
         
-    return sod_arr, l1_arr, l2_arr
+    return sats_arr, ora_arr, sod_arr, l1_arr, l2_arr, c1_arr, RINEX
+################################################################################   
+def interval(obs_file):   ### DEBUGGG AND TEST
+    '''
+    Function that returns the inteval of the obs.
+    '''
+    f   = open(obs_file, "r")
+    lns = f.readlines()
+    f.close()    
+    flag = 0
+    count = 0
+    while "END OF HEADER" not in lns[count]:
+        count += 1
+    count += 1
+   
+    for s in xrange(0, count):
+        if "INTERVAL" in lns[s]:
+            inter = re.findall('\S+\.\S+',lns[s])
+            interval = float(inter[0]) 
+            flag = 1
+            break
+            
+    if flag == 0:
+        sod = []
+        for i in xrange(count,len(lns)):  
+            if "COMMENT" in lns[i]:
+                continue 
+            else:
+                pass  
+        # START stocking the obs                           
+            ln = lns[i].split()                         # split the line[i]
+            if len(ln) == 8:                           # check the number of the split elements, if equal to 8 is the line with "9G27G01G07G32G...
+                sod.append(int(ln[3])*60*60 + int(ln[4])*60 + float(ln[5][0:3]))  
+        interval = sod[1] - sod[0]      
+       
+    return interval
+################################################################################
+def location(obs_file):
+    '''
+    Function that reads the coordinates of the station.
+    input:
+        - obs_file
+    output:
+        -lat,lon
+    '''
+    f   = open(obs_file, "r")
+    lns = f.readlines()
+    f.close()    
+
+    count = 0
+    while "END OF HEADER" not in lns[count]:
+        count += 1
+    count += 1
+        
+    for s in xrange(0, count):
+        if "Monument location:" in lns[s]:
+            coord = re.findall('\S+\.\S+',lns[s])
+            lat = float(coord[0]) 
+            lon = float(coord[1])   
+    return lat,lon
+###############################################################################    
+def coord_geog(staz):
+    
+    '''
+    input: x,y,z
+    output: Latitude (F) and Longitude (L) WGS84
+    '''
+
+    filename1 = staz
+    f1=open(filename1, "r")
+    lines_1 = f1.readlines()
+    f1.close()
+    # lettura automatica delle coordinate approssimate del ricevitore
+    for i in xrange(0,35):
+            if 'COMMENT' in lines_1[i]:
+                continue 
+            elif 'APPROX' in lines_1[i]:
+                x = float(lines_1[i][1:14])
+                y = float(lines_1[i][15:29])
+                z = float(lines_1[i][29:43])
+            # Longitudine calcolabile senza iterazioni
+            L = np.arctan2(y,x)
+
+            r = (x**2 + y**2)**0.5
+            # parametri WGS84
+            a = 6378137
+            f = 1/298.257223563
+            b = a*(1-f)
+            e = (1-(b**2)/(a**2))**0.5
+
+            # I step
+            # ip h=0
+            F  = np.arctan2(z, r*(1-e**2))
+            Rn = a/((1-(e**2)*(np.sin(F))**2))**0.5
+            # Proviamo 50 iterazioni
+            for i in xrange(0,50):
+                h  = r/np.cos(F) - Rn
+                F  = np.arctan2((z*(Rn+h)),(r*Rn*(1-e**2)+h))
+                Rn = a/((1-(e**2)*(np.sin(F))**2))**0.5
+        
+            L_grad = L/(np.pi)*180
+                F_grad = F/(np.pi)*180
+                break
+    return F_grad, L_grad 
